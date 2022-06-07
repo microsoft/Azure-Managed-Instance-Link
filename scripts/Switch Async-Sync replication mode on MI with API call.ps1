ecf73a27baa35c68e1845b548ddbcaf514268cdc
Danimir Ljepava <danil@microsoft.com>
2022-06-07 12:09:03 +0200

    Update Switch Async-Sync replication mode on MI with API call.ps1

--- scripts/Switch Async-Sync replication mode on MI with API call.ps1
+++ scripts/Switch Async-Sync replication mode on MI with API call.ps1
@@ -1,47 +1,42 @@
+# Run in Azure Cloud Shell
 # ====================================================================================
-# POWERSHELL SCRIPT FOR MANAGED INSTANCE TO SWITCH ASYNC-SYNC REPLICATION MODE
+# POWERSHELL SCRIPT TO SWITCH REPLICATION MODE SYNC-ASYNC ON MANAGED INSTANCE
 # USER CONFIGURABLE VALUES
-# (C) 2021 Managed Instance product group
+# (C) 2021-2022 SQL Managed Instance product group
 # ====================================================================================
-# Enter your Azure Subscription ID
-$SubscriptionID = "<YourSubscriptionID>"
-# Enter your Managed Instance name - example "sqlmi1"
-$ManagedInstanceName = "<YourManagedInstanceName>"
-# Enter AG name that was created on the SQL Server
-$DAGName = "<YourDAGName>"
+# Enter your Azure subscription ID
+$SubscriptionID = "<SubscriptionID>"
+# Enter your managed instance name – for example, "sqlmi1"
+$ManagedInstanceName = "<ManagedInstanceName>"
+# Enter the distributed availability group name (the link name)
+$DAGName = "<DAGName>"
 
 # ====================================================================================
 # INVOKING THE API CALL -- THIS PART IS NOT USER CONFIGURABLE
 # ====================================================================================
-# Login to subscription if needed
+# Log in and select a subscription if needed
 if ((Get-AzContext ) -eq $null)
 {
     echo "Logging to Azure subscription"
     Login-AzAccount
 }
 Select-AzSubscription -SubscriptionName $SubscriptionID
-# -----------------------------------
-# Build URI for the API call
-# -----------------------------------
-echo "Building API URI"
+
+# Build a URI for the API call
+#
 $miRG = (Get-AzSqlInstance -InstanceName $ManagedInstanceName).ResourceGroupName
 $uriFull = "https://management.azure.com/subscriptions/" + $SubscriptionID + "/resourceGroups/" + $miRG+ "/providers/Microsoft.Sql/managedInstances/" + $ManagedInstanceName + "/distributedAvailabilityGroups/" + $DAGName + "?api-version=2021-05-01-preview"
 echo $uriFull
-# -----------------------------------
-# Build API request body
-# -----------------------------------
-echo "Building API request body"
-$bodyFull = @"
-{
-	"properties":{
-		"ReplicationMode":"sync"
-	}
-}
-"@
+
+# Build the API request body
+#
+
+$bodyFull = "{`"properties`":{`"ReplicationMode`":`"sync`"}}"
+
 echo $bodyFull 
-# -----------------------------------
-# Get auth token and build the header
-# -----------------------------------
+
+# Get an authentication token and build the header
+#
 $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
 $currentAzureContext = Get-AzContext
 $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)    
@@ -49,9 +44,8 @@ $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
 $authToken = $token.AccessToken
 $headers = @{}
 $headers.Add("Authorization", "Bearer "+"$authToken")
-# -----------------------------------
-# Invoke API call
-# -----------------------------------
-echo "Invoking API call to switch Async-Sync replication mode on Managed Instance."
-$response = Invoke-WebRequest -Method PATCH -Headers $headers -Uri $uriFull -ContentType "application/json" -Body $bodyFull
-echo $response
+
+# Invoke the API call
+#
+echo "Invoking API call switch Async-Sync replication mode on Managed Instance"
+Invoke-WebRequest -Method PATCH -Headers $headers -Uri $uriFull -ContentType "application/json" -Body $bodyFull
