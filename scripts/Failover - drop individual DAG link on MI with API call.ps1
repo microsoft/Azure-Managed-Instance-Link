6ad53a42655a66204faa06d76f363571901c99aa
Danimir Ljepava <danil@microsoft.com>
2022-06-07 12:09:26 +0200

    Update Failover - drop individual DAG link on MI with API call.ps1

--- scripts/Failover - drop individual DAG link on MI with API call.ps1
+++ scripts/Failover - drop individual DAG link on MI with API call.ps1
@@ -1,44 +1,43 @@
+# Run in Azure Cloud Shell
 # ====================================================================================
-# POWERSHELL SCRIPT TO CUTOVER AND DELETE DAG LINK ON MANAGED INSTANCE
+# POWERSHELL SCRIPT TO FAIL OVER AND MIGRATE DATABASE WITH SQL MANAGED INSTANCE LINK
 # USER CONFIGURABLE VALUES
-# (C) 2021 Managed Instance product group
+# (C) 2021-2022 SQL Managed Instance product group
 # ====================================================================================
-# Enter your Azure Subscription ID
+# Enter your Azure subscription ID
 $SubscriptionID = "<SubscriptionID>"
-# Enter your Managed Instance name – example "sqlmi1"
+# Enter your managed instance name – for example, "sqlmi1"
 $ManagedInstanceName = "<ManagedInstanceName>"
-# Enter the DAG link name
+# Enter the distributed availability group link name
 $DAGName = "<DAGName>"
 
 # ====================================================================================
-# INVOKING THE API CALL -- THIS PART IS NOT USER CONFIGURABLE
+# INVOKING THE API CALL -- THIS PART IS NOT USER CONFIGURABLE.
 # ====================================================================================
-# Login to subscription if needed
+# Log in and select a subscription if needed
 if ((Get-AzContext ) -eq $null)
 {
     echo "Logging to Azure subscription"
     Login-AzAccount
 }
 Select-AzSubscription -SubscriptionName $SubscriptionID
-# -----------------------------------
-# Build URI for the API call
-# -----------------------------------
-echo "Building API URI"
+
+# Build a URI for the API call
+#
 $miRG = (Get-AzSqlInstance -InstanceName $ManagedInstanceName).ResourceGroupName
 $uriFull = "https://management.azure.com/subscriptions/" + $SubscriptionID + "/resourceGroups/" + $miRG+ "/providers/Microsoft.Sql/managedInstances/" + $ManagedInstanceName + "/distributedAvailabilityGroups/" + $DAGName + "?api-version=2021-05-01-preview"
 echo $uriFull
-# -----------------------------------
-# Get auth token and build the header
-# -----------------------------------
+
+# Get an authentication token and build the header
+#
 $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
 $currentAzureContext = Get-AzContext
-$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)    
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
 $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
 $authToken = $token.AccessToken
 $headers = @{}
 $headers.Add("Authorization", "Bearer "+"$authToken")
-# -----------------------------------
-# Invoke API call
-# -----------------------------------
-echo "Invoking API call to cutover and delete DAG link on Managed Instance."
+
+# Invoke the API call
+#
 Invoke-WebRequest -Method DELETE -Headers $headers -Uri $uriFull -ContentType "application/json"
