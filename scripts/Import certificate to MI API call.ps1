42f8397f26dfe43bde2a69a1d790675fb376229d
Danimir Ljepava <danil@microsoft.com>
2022-06-07 12:07:20 +0200

    Update Import certificate to MI API call.ps1

--- scripts/Import certificate to MI API call.ps1
+++ scripts/Import certificate to MI API call.ps1
@@ -1,58 +1,56 @@
-# ====================================================================================
+# Run in Azure Cloud Shell
+# ===============================================================================
 # POWERSHELL SCRIPT TO IMPORT SQL SERVER CERTIFICATE TO MANAGED INSTANCE
 # USER CONFIGURABLE VALUES
-# (C) 2021 Managed Instance product group
-# ====================================================================================
-# Enter your Azure Subscription ID
+# (C) 2021-2022 SQL Managed Instance product group
+# ===============================================================================
+# Enter your Azure subscription ID
 $SubscriptionID = "<YourSubscriptionID>"
-# Enter your Managed Instance name - example "sqlmi1"
+
+# Enter your managed instance name – for example, "sqlmi1"
 $ManagedInstanceName = "<YourManagedInstanceName>"
-# Insert the cert public key blob you got from the SQL Server
+
+# Enter the name for the server trust certificate – for example, "Cert_sqlserver1_endpoint"
+$certificateName = "<YourServerTrustCertificateName>"
+
+# Insert the certificate public key blob that you got from SQL Server – for example, "0x1234567..."
+
 $PublicKeyEncoded = "<PublicKeyEncoded>"
 
-# ====================================================================================
-# INVOKING THE API CALL -- THIS PART IS NOT USER CONFIGURABLE
-# ====================================================================================
-# Login to subscription if needed
+# ===============================================================================
+# INVOKING THE API CALL -- REST OF THE SCRIPT IS NOT USER CONFIGURABLE
+# ===============================================================================
+# Log in and select a subscription if needed.
+#
 if ((Get-AzContext ) -eq $null)
 {
     echo "Logging to Azure subscription"
     Login-AzAccount
 }
 Select-AzSubscription -SubscriptionName $SubscriptionID
-# -----------------------------------
-# Build URI for the API call
-# -----------------------------------
-echo "Building API URI"
+
+# Build the URI for the API call.
+#
 $miRG = (Get-AzSqlInstance -InstanceName $ManagedInstanceName).ResourceGroupName
-$uriFull = "https://management.azure.com/subscriptions/" + $SubscriptionID + "/resourceGroups/" + $miRG+ "/providers/Microsoft.Sql/managedInstances/" + $ManagedInstanceName + "/hybridCertificate?api-version=2020-11-01-preview"
+$uriFull = "https://management.azure.com/subscriptions/" + $SubscriptionID + "/resourceGroups/" + $miRG+ "/providers/Microsoft.Sql/managedInstances/" + $ManagedInstanceName + "/serverTrustCertificates/" + $certificateName + "?api-version=2021-08-01-preview"
 echo $uriFull
 
-# -----------------------------------
-# Build API request body
-# -----------------------------------
-echo "Building API request body"
-$bodyFull = @"
-{           
-    "properties":{
-         "PublicBlob":"$PublicKeyEncoded"
-}}
-"@
+# Build the API request body.
+#
+$bodyFull = "{ `"properties`":{ `"PublicBlob`":`"$PublicKeyEncoded`" } }"
+
 echo $bodyFull 
 
-# -----------------------------------
-# Get auth token and build the header
-# -----------------------------------
+# Get auth token and build the HTTP request header.
+#
 $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
 $currentAzureContext = Get-AzContext
-$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)    
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
 $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
 $authToken = $token.AccessToken
 $headers = @{}
 $headers.Add("Authorization", "Bearer "+"$authToken")
 
-# -----------------------------------
 # Invoke API call
-# -----------------------------------
-echo "Invoking API call to import SQL Server certificate to Managed Instance."
-Invoke-WebRequest -Method POST -Headers $headers -Uri $uriFull -ContentType "application/json" -Body $bodyFull
+#
+Invoke-WebRequest -Method PUT -Headers $headers -Uri $uriFull -ContentType "application/json" -Body $bodyFull
